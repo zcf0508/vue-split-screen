ea9cb079f0072a5447ef3804c0082cdcce1fcd69
zcf0508 <zcf0508@live.com>
2023-04-24 16:22:14 +0800

    fix: test error

--- playground/tests/e2e/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("basic test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  const menus = await page.locator("a span").allTextContents();
-  await expect(menus[0]).toBe("Go to Home");
-  await expect(menus[1]).toBe("Go to About");
-});
